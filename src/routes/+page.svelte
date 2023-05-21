ed2355ca3fb74ed4b10821a1fb843ecfa63594df
MonteBro <czech.illusion@gmail.com>
2023-05-21 18:33:51 +0200

    Homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Absence from "$lib/components/Absence.svelte"
+    import CreateUser from "$lib/components/CreateUser.svelte"
+    import Upload from "$lib/components/Upload.svelte"
+</script>
+
+<Upload />
+<Absence />
